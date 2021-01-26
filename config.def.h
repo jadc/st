7ade91f3087c80577e77940c5b689f010c681fe9
jadc <chehimj@gmail.com>
2021-01-25 21:54:43 -0700

    twemoji support

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Fira Code:pixelsize=20:antialias=true:autohint=true";
+static char *font2[] = {"Twemoji:pixelsize=20:antialias=true:autohint=true"};
 static int borderpx = 32;
 
 /*
