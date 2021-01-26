0d804b1701400ae84b2eba47b5d92fc2fd5ec8a2
jadc <chehimj@gmail.com>
2021-01-25 21:27:07 -0700

    fixed font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code Retina:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Fira Code:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 32;
 
 /*
